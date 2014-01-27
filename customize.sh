d9caba63f11d2f3350cf26741ad7b6da61d82410
Victor Porof <victor.porof@gmail.com>
2014-01-27 09:43:46 +0200

    Use more descriptive parameter names in the replace function

--- customize.sh
+++ customize.sh
@@ -3,33 +3,37 @@
 files='find . -type f -not \( -path "*$0*" -o -path "*git*" -o -path "*skin*" -o -path "*.md*" -o -ipath "*make*" \)'
 
 function replace {
-  if [[ $1 == $2 ]]; then
-    echo "The add-on already uses this $3."
+  fieldName=$1
+  oldValue=$2
+  newValue=$3
+
+  if [[ $oldValue == $newValue ]]; then
+    echo "The add-on already uses this $fieldName."
     exit 1
   fi
-  if [ -z "`eval $files | xargs egrep $1 -l`" ]; then
-    echo "The addon's $3 was already customized."
+  if [ -z "`eval $files | xargs egrep $oldValue -l`" ]; then
+    echo "The addon's $fieldName was already customized."
     exit 1
   fi
 
-  read -p "This add-on's $3 will be changed from '$1' to '$2'. Continue? (y/n)? "
+  read -p "This add-on's $fieldName will be changed from '$oldValue' to '$newValue'. Continue? (y/n)? "
   if [[ $REPLY == "y" ]]; then
-    eval $files -print0 | xargs -0 sed -i '' 's/'$1'/'$2'/g'
+    eval $files -print0 | xargs -0 sed -i '' 's/'$oldValue'/'$newValue'/g'
     printf "Done. Modified files:\n\n"
-    eval $files | xargs egrep $2 -l
+    eval $files | xargs egrep $newValue -l
     printf "\nMake sure you also update the extension proxy file's name.\nRefer to the README.md for more information.\n"
   else
-    echo "No changes were made to the addon's $3."
+    echo "No changes were made to the addon's $fieldName."
   fi
 }
 
 while getopts "i:n:" opt; do
   case $opt in
     i)
-      replace "my-addon" $OPTARG "id"
+      replace "id" "my-addon" $OPTARG
       ;;
     n)
-      replace "MyAddon" $OPTARG "name"
+      replace "name" "MyAddon" $OPTARG
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
