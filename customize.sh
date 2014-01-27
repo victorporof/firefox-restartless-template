453d56c0cb8f5746b2ba1d93d5c7f9062e77aa53
Victor Porof <victor.porof@gmail.com>
2014-01-27 10:26:24 +0200

    Add an option to modify the add-on's description

--- customize.sh
+++ customize.sh
@@ -6,34 +6,52 @@ function replace {
   fieldName=$1
   oldValue=$2
   newValue=$3
+  verbose=$4
+  forced=$5
 
-  if [[ $oldValue == $newValue ]]; then
-    echo "The add-on already uses this $fieldName."
-    exit 1
-  fi
-  if [ -z "`eval $files | xargs egrep $oldValue -l`" ]; then
-    echo "The addon's $fieldName was already customized."
-    exit 1
+  if [[ $forced -ne 1 ]]; then
+    if [[ $oldValue == $newValue ]]; then
+      echo "The add-on already uses this $fieldName."
+      exit 1
+    fi
+    if [ -z "`eval $files | xargs egrep $oldValue -l`" ]; then
+      echo "The addon's $fieldName was already customized."
+      exit 1
+    fi
   fi
 
-  read -p "This add-on's $fieldName will be changed from '$oldValue' to '$newValue'. Continue? (y/n)? "
+  if [[ $verbose -ne 1 ]]; then
+    read -p "This add-on's $fieldName will be changed. Continue? (y/n)? "
+  else
+    read -p "This add-on's $fieldName will be changed from '$oldValue' to '$newValue'. Continue? (y/n)? "
+  fi
+
   if [[ $REPLY == "y" ]]; then
-    eval $files -print0 | xargs -0 sed -i '' 's/'$oldValue'/'$newValue'/g'
-    printf "Done. Modified files:\n\n"
-    eval $files | xargs egrep $newValue -l
-    printf "\nMake sure you also update the extension proxy file's name.\nRefer to the README.md for more information.\n"
+    eval $files -print0 | xargs -0 sed -i '' 's/'"$oldValue"'/'"$newValue"'/g'
+
+    if [ $verbose == "1" ]; then
+      printf "Done. Modified files:\n\n"
+      eval $files | xargs egrep $newValue -l
+    fi
+    if [[ $fieldName == "id" ]]; then
+      printf "\nMake sure you also update the extension proxy file's name.\nRefer to the README.md for more information.\n"
+    fi
   else
     echo "No changes were made to the addon's $fieldName."
   fi
 }
 
-while getopts "i:n:" opt; do
+while getopts "i:n:d:" opt; do
   case $opt in
     i)
-      replace "id" "my-addon" $OPTARG
+      replace "id" "my-addon" $OPTARG 1
       ;;
     n)
-      replace "name" "MyAddon" $OPTARG
+      replace "name" "MyAddon" $OPTARG 1
+      ;;
+    d)
+      regex="\(<em:description>\).*\(<\/em:description>\)"
+      replace "description" $regex '\1'"$OPTARG"'\2' 0 1
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
