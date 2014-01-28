7fb571d8217e36bcd098b96cc9054bdc82e1c6c6
Victor Porof <victor.porof@gmail.com>
2014-01-28 09:51:40 +0200

    Document every method in this file

--- chrome/tool.js
+++ chrome/tool.js
@@ -5,7 +5,6 @@
 
 const { classes: Cc, interfaces: Ci, utils: Cu, results: Cr } = Components;
 
-Cu.import("resource://gre/modules/Services.jsm");
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
 XPCOMUtils.defineLazyModuleGetter(this, "EventEmitter",
@@ -13,10 +12,27 @@ XPCOMUtils.defineLazyModuleGetter(this, "EventEmitter",
 XPCOMUtils.defineLazyModuleGetter(this, "promise",
   "resource://gre/modules/commonjs/sdk/core/promise.js", "Promise");
 
+/**
+ * This file has access to the `window` and `document` objects of the add-on's
+ * iframe, and is included in tool.xul. This is the add-on's controller.
+ */
+
+/**
+ * Called when the user select the tool tab.
+ *
+ * @return object
+ *         A promise that should be resolved when the tool completes opening.
+ */
 function startup() {
-  return promise.resolve(null);
+  return promise.resolve();
 }
 
+/**
+ * Called when the user closes the toolbox or disables the add-on.
+ *
+ * @return object
+ *         A promise that should be resolved when the tool completes closing.
+ */
 function shutdown() {
-  return promise.resolve(null);
+  return promise.resolve();
 }
